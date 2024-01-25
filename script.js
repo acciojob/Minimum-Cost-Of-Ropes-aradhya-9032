3b28b91449ca74da12ce93a8f93e752772e2c8de
Aradhya <120170628+aradhya-9032@users.noreply.github.com>
2024-01-25 12:23:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 function calculateMinCost() {
-  //your code here
-var res = 0;
-	var cost = 0;
-	var inputArray = document.getElementById("rope-lengths").value;
+  var res = 0;
+  var cost = 0;
+  var inputArray = document.getElementById("rope-lengths").value;
 
-	inputArray = inputArray.split(',').map(Number);
-	console.log(inputArray);
-	for(let i = 0;i< inputArray.length -1;i+=0) {
-		if (inputArray.length == 0) break;
-		inputArray = inputArray.sort((a,b) => b-a);
-		res = inputArray.pop()+ inputArray.pop();
-		cost += res;
-		inputArray[inputArray.length] = res; }
+  inputArray = inputArray.split(',').map(Number);
+  
+  while (inputArray.length > 1) {
+    // Find the two smallest elements
+    inputArray.sort((a, b) => a - b);
+    
+    // Get the sum of the two smallest elements
+    res = inputArray.shift() + inputArray.shift();
+    
+    // Add the sum to the cost
+    cost += res;
+    
+    // Add the sum back to the array
+    inputArray.push(res);
+  }
 
-	document.getElementById("result").innerHTML = cost;
-	}
+  document.getElementById("result").innerHTML = cost;
+}
